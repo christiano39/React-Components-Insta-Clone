d6b518e5200816bc0fe2f086f68a104196961f60
christiano39 <carneson39@gmail.com>
2020-06-09 13:21:48 -0600

    working on adding ability to add comments

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -2,13 +2,17 @@
 import React from 'react';
 
 const CommentInput = props => {
+  const changeHandler = event => {
+    props.changeComment(event.target.value)
+  }
+
   return (
     <form className="comment-form" onSubmit={props.submitComment}>
       <input
         type="text"
         value={props.comment}
         placeholder="Add comment... "
-        onChange={props.changeComment}
+        onChange={changeHandler}
       />
     </form>
   );
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -7,12 +7,20 @@ import "./Comment.css";
 const CommentSection = props => {
   // Add state for the comments
   const [comments, setComments] = useState(props.comments)
-  // const addComment = comment => setComments(comments.concat(comment))
+  const [currentComment, setCurrentComment] = useState('')
+  const addComment = () => {
+    setComments([...comments, {
+      username: 'christiano39',
+      text: currentComment
+    }])
+    console.log(currentComment)
+    console.log(comments)
+  }
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
       {comments.map((comment, i) => <Comment comment={comment} key={i}/>)}
-      <CommentInput />
+      <CommentInput changeComment={setCurrentComment} submitComment={addComment}/>
     </div>
   );
 };
